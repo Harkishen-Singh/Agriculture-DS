3fe57f8f7f173f40c31ee1a8bf8c38b589383b70
Harkishen Singh <harkishensingh@hotmail.com>
2018-03-05 19:41:13 +0530

    first stable form

--- manures.py
+++ manures.py
@@ -1,37 +1,35 @@
 from Process import Process
 from openpyxl import load_workbook
 
+
 class Manures(Process):
     def avg_Good(self):
         rows = 2
         file = load_workbook(self.sortedFileName_Good)
-        print('file addres of good '+ self.sortedFileName_Good)
         sheet = file['Sorted']
-        while(True):
+        while (True):
             if sheet.cell(row=rows, column=2).value != None:
                 rows = rows + 1
             else:
                 print('came here')
                 break
 
-        print('rows : '+ str(rows))
-        total_chemfer = 0; c_chemfer = 0
-        total_manure = 0 ; c_manure = 0
+        total_chemfer = 0;
+        c_chemfer = 0
+        total_manure = 0;
+        c_manure = 0
         file2 = load_workbook('./ratios/ratio_good.xlsx')
         sheet2 = file2['Sheet']
-        sheet2.cell(row= 1,column=5, value='ChemFer_Ratio')
+        sheet2.cell(row=1, column=5, value='ChemFer_Ratio')
         sheet2.cell(row=1, column=6, value='Manure_Ratio')
         varietyCount = 2
 
         for i in range(2, rows):
 
-            
+            if sheet.cell(row=i, column=8).value == sheet.cell(row=i + 1, column=8).value:
 
-            if sheet.cell(row = i, column=8).value == sheet.cell(row = i+1, column=8).value :
-                print('But it reached here at '+str(i))
-
-                if sheet.cell(row=i, column=12).value == None:
-                    continue
+                if sheet.cell(row=i, column=12).value == None :
+                    c_chemfer = c_chemfer + 1
 
                 elif type(sheet.cell(row=i, column=12).value) == str:
                     number = ''
@@ -45,7 +43,7 @@ class Manures(Process):
                     c_chemfer = c_chemfer + 1
 
                 if sheet.cell(row=i, column=11).value == None:
-                    continue
+                    c_manure = c_manure + 1
 
                 elif type(sheet.cell(row=i, column=11).value) == str:
                     number = ''
@@ -62,7 +60,6 @@ class Manures(Process):
                     sheet.cell(row=i, column=8).value != sheet.cell(row=i - 1, column=8).value:
                 if sheet.cell(row=i, column=12).value == None:
                     continue
-
                 elif type(sheet.cell(row=i, column=12).value) == str:
                     number = ''
                     for j in sheet.cell(row=i, column=12).value:
@@ -73,10 +70,8 @@ class Manures(Process):
                 else:
                     total_chemfer = total_chemfer + float(sheet.cell(row=i, column=12).value)
                     c_chemfer = c_chemfer + 1
-
                 if sheet.cell(row=i, column=11).value == None:
                     continue
-
                 elif type(sheet.cell(row=i, column=11).value) == str:
                     number = ''
                     for j in sheet.cell(row=i, column=11).value:
@@ -89,10 +84,10 @@ class Manures(Process):
                     c_manure = c_manure + 1
             '''
 
-            if sheet.cell(row = i, column=8).value != sheet.cell(row = i+1, column=8).value :
+            if sheet.cell(row=i, column=8).value != sheet.cell(row=i + 1, column=8).value:
 
                 if sheet.cell(row=i, column=12).value == None:
-                    continue
+                    c_chemfer = c_chemfer + 1
 
                 elif type(sheet.cell(row=i, column=12).value) == str:
                     number = ''
@@ -106,7 +101,7 @@ class Manures(Process):
                     c_chemfer = c_chemfer + 1
 
                 if sheet.cell(row=i, column=11).value == None:
-                    continue
+                    c_manure = c_manure + 1
 
                 elif type(sheet.cell(row=i, column=11).value) == str:
                     number = ''
@@ -119,17 +114,21 @@ class Manures(Process):
                     total_manure = total_manure + float(sheet.cell(row=i, column=11).value)
                     c_manure = c_manure + 1
 
-                print('Reached here at '+ str(i))
-                print('VarietyCOunt at '+ str(varietyCount))
+                print(str(sheet2.cell(row=varietyCount, column=4).value) + ' '+ str(total_chemfer / c_chemfer) + ' '
+                      + str(total_manure / c_manure) + ' '
+                      + str(sheet.cell(row= i, column=8).value))
                 sheet2.cell(row=varietyCount, column=5, value=total_chemfer / c_chemfer)
                 sheet2.cell(row=varietyCount, column=6, value=total_manure / c_manure)
+                sheet2.cell(row=varietyCount, column=7, value=sheet.cell(row= i, column=8).value)
                 varietyCount = varietyCount + 1
-                total_manure = 0; c_manure = 0
-                total_chemfer = 0; c_chemfer = 0
-
-
+                total_manure = 0;
+                c_manure = 0
+                total_chemfer = 0;
+                c_chemfer = 0
 
         file2.save('./ratios/ratio_good.xlsx')
+
+
 
 obj = Manures()
 obj.individualAllotment()
